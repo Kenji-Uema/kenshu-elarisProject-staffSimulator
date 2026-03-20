11c29bb010f42796093bb59f11dba889b225dbca
Kenji Uema <kenji.uema.developer@pm.me>
2026-03-19 22:27:50 -0300

    removed unused functions

--- integration_test/helpers/network_helper.go
+++ integration_test/helpers/network_helper.go
@@ -15,23 +15,3 @@ func FreeTCPPort(t TestReporter) int {
 
 	return l.Addr().(*net.TCPAddr).Port
 }
-
-func HostOnly(t TestReporter, addr string) string {
-	t.Helper()
-
-	host, _, err := net.SplitHostPort(addr)
-	if err != nil {
-		t.Fatalf("split host from %q: %v", addr, err)
-	}
-	return host
-}
-
-func PortOnly(t TestReporter, addr string) string {
-	t.Helper()
-
-	_, port, err := net.SplitHostPort(addr)
-	if err != nil {
-		t.Fatalf("split port from %q: %v", addr, err)
-	}
-	return port
-}
